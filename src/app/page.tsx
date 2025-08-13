ff0e8792df209af86e8afff38269e932bbb6375b
Yungyingzerza <yungyingzerza2547@gmail.com>
2025-08-14 04:11:00 +0700

    init project & add VideoPlayer Component

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import VideoPlayer from "@/components/VideoPlayer";
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-[90svh]">
+        <VideoPlayer />
+      </div>
+    </>
+  );
+}
